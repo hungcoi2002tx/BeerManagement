90b353cdaaedce7ea0790282f81805e6d60cd5ea
Nguyen Minh Hung <Hackinghungtx@gmail.com>
2024-06-08 16:02:04 +0700

    up auto mapper

---
+++ Share/Ultils/MapperConfig.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Ultils
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            #region Auto Map Config
+            var assembly = Assembly.GetAssembly(typeof(MapperConfig));
+            var classes = assembly.ExportedTypes.Where(x => x.Namespace.Equals("Share.Models.Domain"));
+            foreach (var type in classes)
+            {
+                CreateMap(type, type).ReverseMap();
+                var editModelClass = assembly.ExportedTypes.FirstOrDefault(x => x.Name == type.Name + "EditModel");
+                if (editModelClass != null)
+                {
+                    CreateMap(type, editModelClass).ReverseMap();
+                }
+                var viewModelClass = assembly.ExportedTypes.FirstOrDefault(x => x.Name == type.Name + "ViewModel");
+                if (viewModelClass != null)
+                {
+                    CreateMap(type, viewModelClass).ReverseMap();
+                }
+            }
+            #endregion
+
+            #region Custome Map
+            #endregion
+        }
+    }
+}
---
+++ Share/Ultils/ServiceExtentions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share.Ultils
+{
+    public static class ServiceExtentions
+    {
+        public static void AddService(this IServiceCollection services)
+        {
+            #region add services
+            #endregion
+        }
+    }
+}
