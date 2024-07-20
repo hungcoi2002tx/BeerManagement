963778effa9a4d2689cb53302fa43125fe1e9f97
thaiLA <leanthai02@gmail.com>
2024-07-20 21:01:28 +0700

    add datalayer for order

--- DataLayer/Interfaces/IOrderRepository.cs
+++ DataLayer/Interfaces/IOrderRepository.cs
@@ -9,7 +9,7 @@ namespace DataLayer.Interfaces
 {
     public interface IOrderRepository : IRepository<Share.Models.Domain.Order>
     {
-        Task<(List<Share.Models.Domain.Order>, int)> GetPageBySearchAsync(OrderSearchDto tableSearchDto);
+        Task<(List<Share.Models.Domain.Order>, int)> GetPageBySearchAsync(OrderSearchDto obj);
         Task<bool> UpdateAsync(Share.Models.Domain.Order model);
     }
 }
